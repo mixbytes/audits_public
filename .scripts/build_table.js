8ed5373028b095aefeb9e440c7a94906a01fe317
Algys Ievlev <algys.ievlev@gmail.com>
2021-09-20 16:19:05 +0300

    add auto updates via github actions

--- .scripts/build_table.js
+++ .scripts/build_table.js
@@ -3,9 +3,16 @@ const pdf = require("pdf-parse");
 const fs = require("fs");
 const moment = require('moment');
 
+const BASE_URL = 'https://github.com/mixbytes/audits_public/blob/master/';
+const README_TEMPLATE_PATH = __dirname + '/main_readme_template.md';
+const MAIN_README_PATH = __dirname + '/../README.md';
+
+
 (async () => {
 
-const mdFiles = glob.sync("*/**/+(readme|README).md").map(file => file.split('/')).filter(file => file.length > 1);
+const mdFiles = glob.sync("*/**/+(readme|README).md")
+        .map(file => file.split('/'))
+        .filter(file => file.length > 1);
 
 let audits = mdFiles.map(mdFile => {
     const mdFilePath = mdFile.join('/');
@@ -13,21 +20,15 @@ let audits = mdFiles.map(mdFile => {
     const project = mdFile[0];
     const auditName = mdFile.length > 2 ? mdFile[1] : mdFile[0];
 
-    return [project, auditName, basePath, mdFilePath];
-});
-
-
-
-audits = audits.map(audit => {
-    const files = glob.sync(`${audit[2]}/*.pdf`);
+    let pdfFilePath = null;
+    const files = glob.sync(`${basePath}/*.pdf`);
     if (files.length > 0) {
-        audit.push(files[0])
+        pdfFilePath = files[0];
     }
-    else {
-        audit.push(null);
-    }
-    return audit;
+
+    return [project, auditName, basePath, mdFilePath, pdfFilePath];
 });
+
 
 audits = await Promise.all(audits.map(async (audit) => {
     if (audit[4] != null) {
@@ -49,8 +50,6 @@ audits = await Promise.all(audits.map(async (audit) => {
     return audit;
 }));
 
-console.log(audits);
-
 audits.sort(function(a, b) {
     // Compare the 2 dates
     if (a[5] == 'N/A' && b[5] != 'N/A') return 1;
@@ -63,12 +62,14 @@ audits.sort(function(a, b) {
 let table = '| Project | Audit Name | MD Report | PDF Report | Release Date (YYYY-MM-DD) |\n';
 table +=    '|---|---|---|---|---|\n';
 audits.forEach(audit => {
-    const mdPath = `[link](${encodeURI('./' + audit[3])})`;
-    const pdfPath = audit[4] != null ? `[link](${encodeURI('./' + audit[4])})` : 'N/A';
+    const mdPath = `[link](${encodeURI(BASE_URL + audit[3])})`;
+    const pdfPath = audit[4] != null ? `[link](${encodeURI(BASE_URL + audit[4])})` : 'N/A';
 
     table += `| ${audit[0]} | ${audit[1]} | ${mdPath} | ${pdfPath} | ${audit[5]} |\n`;
 });
 
-console.log(table);
+let readme = fs.readFileSync(README_TEMPLATE_PATH).toString();
+readme = readme.replace('{REPORT_LIST}', table);
+fs.writeFileSync(MAIN_README_PATH, readme);
 
 })().then();
---
+++ .scripts/main_readme_template.md
@@ -0,0 +1,15 @@
+![](MixBytes.png)
+
+# MixBytes Team public audits
+
+# Reports list
+{REPORT_LIST}
+
+## Refresh reports list
+```bash
+(cd .scripts && npm i)
+node .scripts/build_table.js
+```
+
+## Change main readme template
+To change this readme template need to edit [file](.scripts/main_readme_template.md)
